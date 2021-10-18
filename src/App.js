536c0a223ed69c398dbb62f4c2d3bf626a7ef68d
Dayazispl <dayanidhee.singh@zispl.com>
2021-10-17 22:08:14 -0700

    game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Player from "./player";
+import "./App.css";
 
-function App() {
+const weapons = ["rock", "paper", "scissors"];
+const App = () => {
+  const [state, setState] = useState({
+    player: weapons[0],
+    computer: weapons[0],
+    winner: "",
+  });
+  const [score, setScore] = useState(0);
+
+  const startGame = () => {
+    let counter = 0;
+    let gameInterval = setInterval(() => {
+      counter++;
+      setState({
+        ...state,
+        computer: weapons[Math.floor(Math.random() * weapons.length)],
+        winner: "",
+      });
+      if (counter > 5) {
+        clearInterval(gameInterval);
+        setState({
+          ...state,
+          winner: selectWinner(),
+        });
+      }
+    }, 100);
+  };
+
+  useEffect(() => {
+    if (state.winner === "You Won :)") {
+      setScore(score + 1);
+    } else if (state.winner === "You Lose :(") {
+      setScore(score - 1);
+    }
+  }, [state.winner]);
+
+  const selectWinner = () => {
+    const { player, computer } = state;
+
+    if (player === computer) {
+      return "Oops it's a Tie!";
+    } else if (
+      (player === "rock" && computer === "scissors") ||
+      (player === "scissors" && computer === "paper") ||
+      (player === "paper" && computer === "rock")
+    ) {
+      return "You Won :)";
+    } else {
+      return "You Lose :(";
+    }
+  };
+
+  const selectWeapon = (weapon) => {
+    setState({
+      ...state,
+      player: weapon,
+      winner: "",
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 style={{ textAlign: "center" }}>Rock Paper Scissors</h1>
+      <p style={{ textAlign: "center" }}>
+        Click on your choice and then click on start
+      </p>
+      <div>
+        <Player weapon={state.player} />
+        <Player weapon={state.computer} />
+      </div>
+      <div className="buttons">
+        <button className="weaponBtn" onClick={() => selectWeapon("rock")}>
+          rock
+        </button>
+        <button className="weaponBtn" onClick={() => selectWeapon("paper")}>
+          paper
+        </button>
+        <button className="weaponBtn" onClick={() => selectWeapon("scissors")}>
+          scissor
+        </button>
+      </div>
+
+      <button type="button" onClick={startGame} style={{ marginTop: "20px" }}>
+        Start!
+      </button>
+      <div className="winner">{state.winner ? selectWinner() : null}</div>
+      <div className="score">
+        {score === 0 ? "Your Score is 0" : `Your Score is ${score}`}
+      </div>
+    </>
   );
-}
+};
 
 export default App;
